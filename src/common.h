c1f6034e173f328c0b0601607c514a138fc8b36a
Roland Marchand <roland.marchand@protonmail.com>
2023-04-13 14:57:02 -0400

    Added a common include file

---
+++ src/common.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+#include <float.h>
+#include <math.h>
+
+#include "config.h"
+#include "vector.h"
+
+enum Error {
+	OK = 0,
+	ERROR
+};
