cd14565555d3e45ea16465c1b55d03819c03e76a
Roland Marchand <roland.marchand@protonmail.com>
2023-04-19 19:14:53 -0400

    Updated level tests

--- test/test_level.c
+++ test/test_level.c
@@ -1,34 +1,105 @@
 #include "level.h"
 
-wall walls[9];
+extern sector_idx_t LastKnownSector;
+
+void test_inside(void);
+void test_update_sector(void);
 
 int main(void)
 {
-	/* hexagon room */
-	walls[0] = (wall){Line(Vector(64, 128), Vector(128, 0)), -1};
-	walls[1] = (wall){Line(Vector(128, 0), Vector(64, -128)), -1};
-	walls[2] = (wall){Line(Vector(64, -128), Vector(-64, -128)), -1};
-	walls[3] = (wall){Line(Vector(-64, -128), Vector(-128, 0)), -1};
-	walls[4] = (wall){Line(Vector(-128, 0), Vector(-64, 128)), -1};
-	walls[5] = (wall){Line(Vector(-64, 128), Vector(64, 128)), -1};
+	test_update_sector();
+	test_inside();
+}
 
-	/* triangle room */
-	walls[6] = (wall){Line(Vector(0, 64), Vector(64, 0)), -1};
-	walls[7] = (wall){Line(Vector(64, 0), Vector(-64, 0)), -1};
-	walls[8] = (wall){Line(Vector(-64, 0), Vector(0, 64)), -1};
+void test_inside(void)
+{
+	struct wall walls[] = {
+		/* hexagon room */
+		{Line(Vector(64, 128), Vector(128, 0)), INVALID_SECTOR},
+		{Line(Vector(128, 0), Vector(64, -128)), INVALID_SECTOR},
+		{Line(Vector(64, -128), Vector(-64, -128)), INVALID_SECTOR},
+		{Line(Vector(-64, -128), Vector(-128, 0)), INVALID_SECTOR},
+		{Line(Vector(-128, 0), Vector(-64, 128)), INVALID_SECTOR},
+		{Line(Vector(-64, 128), Vector(64, 128)), INVALID_SECTOR},
+		/* triangle room */
+		{Line(Vector(0, 64), Vector(64, 0)), INVALID_SECTOR},
+		{Line(Vector(64, 0), Vector(-64, 0)), INVALID_SECTOR},
+		{Line(Vector(-64, 0), Vector(0, 64)), -1}
+	};
+	struct sector two_rooms[] = {
+		{0, 6}, /* hexagon */
+		{6, 3} /* triangle */
+	};
+	initialize_level(walls, two_rooms, 9, 2);
 
-	sector hexagon = {0, 6};
-	assert(!inside(walls, hexagon, Vector(1000, 1000)));
-	assert(!inside(walls, hexagon, Vector(-1000, 1000)));
-	assert(!inside(walls, hexagon, Vector(1000, -1000)));
-	assert(!inside(walls, hexagon, Vector(-1000, -1000)));
-	assert(inside(walls, hexagon, Vector(0, 64)));
-	assert(inside(walls, hexagon, Vector(0, 0)));
+	/* hexagon */
+	assert(!inside(0, Vector(1000, 1000)));
+	assert(!inside(0, Vector(-1000, 1000)));
+	assert(!inside(0, Vector(1000, -1000)));
+	assert(!inside(0, Vector(-1000, -1000)));
+	assert(inside(0, Vector(0, 64)));
+	assert(inside(0, Vector(0, 0)));
 
-	sector triangle = {6, 3};
-	assert(!inside(walls, triangle, Vector(64, 64)));
-	assert(!inside(walls, triangle, Vector(0, 64)));
-	assert(!inside(walls, triangle, Vector(128, 0)));
-	assert(!inside(walls, triangle, Vector(0, 0)));
-	assert(inside(walls, triangle, Vector(0, 32)));
+	/* triangle */
+	assert(!inside(1, Vector(64, 64)));
+	assert(!inside(1, Vector(0, 64)));
+	assert(!inside(1, Vector(128, 0)));
+	assert(!inside(1, Vector(0, 0)));
+	assert(inside(1, Vector(0, 32)));
+
+	terminate_level();
+}
+
+void test_update_sector(void)
+{
+/*
+  o---+---+
+  | 0 | 1 |
+  +---+---+
+  | 3 | 2 |
+  +---+---+
+*/
+	struct wall walls[] = {
+		/* 0 */
+		{Line(Vector(0, 0), Vector(64, 0)), INVALID_SECTOR},
+		{Line(Vector(64, 0), Vector(64, -64)), 1},
+		{Line(Vector(64, -64), Vector(0, -64)), 3},
+		{Line(Vector(0, -64), Vector(0, 0)), INVALID_SECTOR},
+		/* 1 */
+		{Line(Vector(64, 0), Vector(128, 0)), INVALID_SECTOR},
+		{Line(Vector(128, 0), Vector(128, -64)), INVALID_SECTOR},
+		{Line(Vector(128, -64), Vector(64, -64)), 2},
+		{Line(Vector(64, -64), Vector(64, 0)), 0},
+		/* 2 */
+		{Line(Vector(64, -64), Vector(128, -64)), 1},
+		{Line(Vector(128, -64), Vector(128, -128)), INVALID_SECTOR},
+		{Line(Vector(128, -128), Vector(64, -128)), INVALID_SECTOR},
+		{Line(Vector(64, -128), Vector(64, -64)), 3},
+		/* 3 */
+		{Line(Vector(0, -64), Vector(64, -64)), 0},
+		{Line(Vector(64, -64), Vector(64, -128)), 2},
+		{Line(Vector(64, -128), Vector(0, -128)), INVALID_SECTOR},
+		{Line(Vector(0, -128), Vector(0, -64)), INVALID_SECTOR},
+	};
+	struct sector sectors[] = {
+		{0, 4},
+		{4, 4},
+		{8, 4},
+		{12, 4}
+	};
+	initialize_level(walls, sectors, 16, 4);
+
+	assert(LastKnownSector == INVALID_SECTOR);
+	update_sector(Vector(32, -32));
+	assert(LastKnownSector == 0);
+	update_sector(Vector(32, -32));
+	assert(LastKnownSector == 0);
+	update_sector(Vector(96, -32));
+	assert(LastKnownSector == 1);
+	update_sector(Vector(32, -96));
+	assert(LastKnownSector == 3);
+	update_sector(Vector(-128, 128));
+	assert(LastKnownSector == INVALID_SECTOR);
+
+	terminate_level();
 }
