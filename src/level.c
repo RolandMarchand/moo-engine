a6b63d3145b707cf1b08833ecae25f6b1412639d
Roland Marchand <roland.marchand@protonmail.com>
2023-04-19 19:13:59 -0400

    Added new `update_sector`, `initialize_level` and `terminate_level` functions, and fixed `inside` function

--- src/level.c
+++ src/level.c
@@ -1,44 +1,95 @@
 #include "level.h"
 
-/* wall walls[6] = { */
-/* 	{Line(Vector(64, 128), Vector(128, 0)), -1}, */
-/* 	{Line(Vector(128, 0), Vector(64, -128)), -1}, */
-/* 	{Line(Vector(64, -128), Vector(-64, -128)), -1}, */
-/* 	{Line(Vector(-64, -128), Vector(-128, 0)), -1}, */
-/* 	{Line(Vector(-128, 0), Vector(-64, 128)), -1}, */
-/* 	{Line(Vector(-64, 128), Vector(64, 128)), -1}, */
-/* }; */
+struct wall *Walls;
+struct sector *Sectors;
+size_t WallCount;
+size_t SectorCount;
 
-/* sector sectors[1] = { */
-/* 	{0, 6} */
-/* }; */
+sector_idx_t LastKnownSector = INVALID_SECTOR;
 
-bool inside(wall *walls, sector s, vector point)
+void initialize_level(
+	struct wall *p_walls, struct sector *p_sectors,
+	size_t wall_cnt, size_t sector_cnt)
 {
-	// TODO: check if sector is valid
-	int intersection_count = 0;
-	for (int i = 0; i < s.wall_count; i++) {
-		line wall = walls[s.wall_start + i].line;
+	terminate_level();
+	WallCount = wall_cnt;
+	SectorCount = sector_cnt;
+	Walls = malloc(sizeof(struct wall) * wall_cnt);
+	Sectors = malloc(sizeof(struct sector) * sector_cnt);
+	memcpy(Walls, p_walls, sizeof(struct wall) * wall_cnt);
+	memcpy(Sectors, p_sectors, sizeof(struct sector) * sector_cnt);
+}
 
-		/* handle vertexes */
-		if (wall.a.y == point.y) {
-			if (wall.a.x < point.x && wall.b.y < point.y) {
-					intersection_count++;
-			}
-			continue;
-		} else if (wall.b.y == point.y) {
-			if (wall.b.x < point.x && wall.a.y < point.y) {
-					intersection_count++;
-			}
+void terminate_level()
+{
+	if (Walls) {
+		free(Walls);
+		Walls = NULL;
+	}
+	if (Sectors){
+		free(Sectors);
+		Sectors = NULL;
+	}
+	WallCount = 0;
+	SectorCount = 0;
+}
+
+void update_sector(vector updated_pos)
+{
+	if (LastKnownSector == INVALID_SECTOR) {
+		goto scan_all;
+	}
+
+	if (inside(LastKnownSector, updated_pos)) {
+		return;
+	}
+
+	/* TODO: Optimize so that the same sector isn't checked more
+	 * than once */
+	/* Scan neighboring sectors */
+	struct sector last_sector = Sectors[LastKnownSector];
+	for (wall_idx_t idx = 0; idx < last_sector.wall_count; idx++) {
+		struct wall wall = Walls[last_sector.wall_start + idx];
+		sector_idx_t next_sector = wall.next_sector;
+		if (next_sector == INVALID_SECTOR) {
 			continue;
 		}
+		if (inside(next_sector, updated_pos)) {
+			LastKnownSector = next_sector;
+			return;
+		}
+	}
+
+  scan_all:
+	for (size_t sector_idx = 0; sector_idx < SectorCount; sector_idx++) {
+		if (inside(sector_idx, updated_pos)) {
+			LastKnownSector = sector_idx;
+			return;
+		}
+	}
+	LastKnownSector = INVALID_SECTOR;
+}
+
+bool inside(sector_idx_t idx, vector point)
+{
+	if (idx == INVALID_SECTOR) {
+		return false;
+	}
+
+	/* TODO: check if sector is valid */
+	int intersection_count = 0;
+	struct sector s = Sectors[idx];
+	for (size_t i = 0; i < s.wall_count; i++) {
+		line wall = Walls[s.wall_start + i].line;
 
 		double min_x = wall.a.x < wall.b.x ? wall.a.x : wall.b.x;
 		vector crossed = line_line_intersection(
-			wall, Line(Vector(min_x, point.y), point));
+			wall, Line(Vector(min_x - 1, point.y), point));
 
 		if (VectorIsValid(crossed)) {
-			intersection_count++;
+			if (wall.a.y < point.y || wall.b.y < point.y) {
+				intersection_count++;
+			}
 		}
 	}
 	return intersection_count % 2 == 1;
