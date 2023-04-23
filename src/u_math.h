f888f660096da0535622dccd9181c1dc6beb5fd4
Roland Marchand <roland.marchand@protonmail.com>
2023-04-22 20:23:08 -0400

    Replacing useless PI constant in favor of glibc's M_PI, added angle conversion macros

--- src/u_math.h
+++ src/u_math.h
@@ -4,8 +4,6 @@
 #include "math.h"
 #include "vector.h"
 
-#define PI 3.141592654
-
 #define clamp(_x, _min, _max) (SDL_min(SDL_max(_x, _min), _max))
 #define swap(_a, _b)				\
 	do {					\
@@ -13,3 +11,5 @@
 		_a = (_b);			\
 		_b = (_x);			\
 	} while (0)
+#define deg2rad(_deg) ((_deg) * M_PI / 180.0)
+#define rad2deg(_rad) ((_rad) * 180.0 / M_PI)
