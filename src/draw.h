5f63cb3bc7e3d6b120cdb9f911fb0def2d91d215
Roland Marchand <roland.marchand@protonmail.com>
2023-04-28 19:32:57 -0400

    Updated color structure

--- src/draw.h
+++ src/draw.h
@@ -4,25 +4,14 @@
 #include "common.h"
 #include "u_math.h"
 
-#define Color(_r, _g, _b) ((struct color){ .r = _r, .g = _g, .b = _b})
+#define Color(_r, _g, _b) ((color){_r, _g, _b, 255})
+#define Colora(_r, _g, _b, _a) ((color){_r, _g, _b, _a})
 
-struct color {
-	union {
-		Uint8 red;
-		Uint8 r;
-	};
-	union {
-		Uint8 green;
-		Uint8 g;
-	};
-	union {
-		Uint8 blue;
-		Uint8 b;
-	};
-};
+typedef SDL_Color color;
 
-void draw_line(Uint32 *pixels, line, SDL_Color);
-void draw_pixel(Uint32 *pixels, int pixel_x, int pixel_y, SDL_Color c);
-void draw_rect(Uint32 *pixels, vector p0, vector p1, SDL_Color);
-void draw_triangle(Uint32 *pixels, vector a, vector b, vector c, SDL_Color);
-void draw_quad(Uint32 *pixels, vector p1, vector p2, vector p3, vector p4, SDL_Color c);
+void draw_line(Uint32 *pixels, line, color);
+void draw_pixel(Uint32 *pixels, int pixel_x, int pixel_y, color);
+void draw_rect(Uint32 *pixels, vector p0, vector p1, color);
+void draw_triangle(Uint32 *pixels, vector a, vector b, vector c, color);
+void draw_quad(Uint32 *pixels, vector p1, vector p2, vector p3, vector p4, color);
+void draw_background(SDL_Surface *surface, color);
